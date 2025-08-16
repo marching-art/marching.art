fcd2e33227d9593c756359e7bc9ac16a3e16db01
Chris Rohn <chrisrohn@gmail.com>
2025-08-16 11:20:26 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // This is the line that enables dark mode
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
