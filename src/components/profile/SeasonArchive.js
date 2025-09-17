ee02ecabc69358d615374b234625a852bccc7087
Chris Rohn <chrisrohn@gmail.com>
2025-09-17 12:33:30 -0400

    Add files via upload

--- src/components/profile/SeasonArchive.js
+++ src/components/profile/SeasonArchive.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
 // HELPER FUNCTION: This function reads the current theme's colors from the CSS variables on the root element.
 const getThemeColors = () => {
