8943849b11b37496563799f91fe5b8d6361a913b
Chris Rohn <chrisrohn@gmail.com>
2025-09-22 22:00:09 -0400

    Delete src/pages/ProfuleSetupPage.js

--- src/pages/ProfuleSetupPage.js
+++
@@ -1,9 +0,0 @@
-// src/pages/ProfileSetupPage.js
-import React from 'react';
-import ProfileSetup from '../components/auth/ProfileSetup';
-
-const ProfileSetupPage = () => {
-    return <ProfileSetup />;
-};
-
-export default ProfileSetupPage;
