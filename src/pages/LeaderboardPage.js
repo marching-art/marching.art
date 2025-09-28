77652d7231b6b5bb9d95a67fcfba7bab54a565bf
Chris Rohn <chrisrohn@gmail.com>
2025-09-28 16:33:22 -0400

    feat: Add placeholder pages for all routes

---
+++ src/pages/LeaderboardPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeaderboardPage = () => {
+  return (
+    <div>
+      <h1>Leaderboard Page</h1>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
---
+++ src/pages/LeaguesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeaguesPage = () => {
+  return (
+    <div>
+      <h1>Leagues Page</h1>
+    </div>
+  );
+};
+
+export default LeaguesPage;
